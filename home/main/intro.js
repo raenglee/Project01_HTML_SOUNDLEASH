a38375371d89d2704aa844533689d0c4d240140f
raenglee <gusdl199@naver.com>
2024-08-22 15:28:21 +0900

    240822 필요없는 파일 삭제

--- home/main/intro.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.bar').forEach(bar => {
-    bar.addEventListener('click', () => {
-        const url = bar.getAttribute('data-url');
-        window.location.href = url;
-    });
-});
